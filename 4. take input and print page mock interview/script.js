f7c0cf82d064d89a8000393dca299e097306085b
DipakFarate <96985671+DipakFarate@users.noreply.github.com>
2022-03-20 07:28:24 +0530

    Add files via upload

---
+++ 4. take input and print page mock interview/script.js
@@ -0,0 +1,7 @@
+document.getElementById("myButton").onclick =function(){
+    var firstname =document.getElementById("name").value;
+    var femail =document.getElementById("email").value;
+    var fmobile =document.getElementById("number").value;
+
+    (here.innerHTML =firstname+" "+femail+" "+fmobile);
+}
