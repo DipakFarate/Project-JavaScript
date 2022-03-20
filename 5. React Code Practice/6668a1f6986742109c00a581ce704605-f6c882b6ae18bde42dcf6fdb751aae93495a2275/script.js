3df20342469c7146081f0cc43d161760236cd50b
DipakFarate <96985671+DipakFarate@users.noreply.github.com>
2022-03-20 07:29:00 +0530

    Add files via upload

---
+++ 5. React Code Practice/6668a1f6986742109c00a581ce704605-f6c882b6ae18bde42dcf6fdb751aae93495a2275/script.js
@@ -0,0 +1,35 @@
+// using alert show and print mag on disply screen and popup box
+// alert("wlcome to my chanel");
+// document.write("Dipak farate");
+
+
+var a =10;
+var b =12;
+var c =a+b;
+// br tag used gap between two lines
+document.write("<br>")
+document.write(c)
+
+
+
+
+//     inside double coates one more coates  then use back slash show below
+var youtube ="you are a learning \"Newton school\" ";
+document.write(youtube);
+
+
+
+// typeof() used for which type of data is show
+var numb =100;
+document.write(typeof(numb))
+
+
+// different types of oerator
+// Assignment Operator
+var a = 10;
+var b = 20;
+var sum =a+b;
+document.write(sum);
+
+
+
